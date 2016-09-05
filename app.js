e95c856887d6f6f07dfc0d1fd10482a0f4c1b4c5
Daniel García Jones <daniel.garcia@es.unisys.com>
2016-09-05 15:20:02 +0200

    X-origin

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ var loanRouter = require('./routes/loanRouter');
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, X-Auth-Token, Content-Type, Accept, Authorization");
   next();
 });
 
