d31cf054a1decbd198ff7c25bcd11e1cbb90753d
Daniel García Jones <daniel.garcia@es.unisys.com>
2016-09-16 22:14:47 +0200

    Updated config file and added README

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
+//REST API Configuration Parameters
 module.exports = {
     'secretKey': '12345-67890-09876-54321', //Put a secure Key here
-    'mongoUrl' : 'mongodb://localhost:27017/smt_db' //Put the URL to the DB
+    'mongoUrl' : 'mongodb://imtuser:Unisys01@ds033126.mlab.com:33126/imt_db' //Put the URL to the DB
 }
