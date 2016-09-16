3c48e8cf44139aa2a008ba3c34d0fbb0ca17e0a4
Daniel García Jones <daniel.garcia@es.unisys.com>
2016-09-16 22:17:05 +0200

    Refactor

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 //REST API Configuration Parameters
 module.exports = {
     'secretKey': '12345-67890-09876-54321', //Put a secure Key here
-    'mongoUrl' : 'mongodb://imtuser:Unisys01@ds033126.mlab.com:33126/imt_db' //Put the URL to the DB
+    'mongoUrl' : 'put_your_url_to_db' //Put the URL to the DB
 }
